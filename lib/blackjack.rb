c6043f8a262ab8f502735f1df7982b781f526d2f
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 01:28:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,9 +24,10 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_1= deal_card
-  deal_card
-  display_card_total
+  card_1 = deal_card
+  card_2 = deal_card
+  total = card_1 + card_2
+  display_card_total(total)
 end
 
 def hit?
